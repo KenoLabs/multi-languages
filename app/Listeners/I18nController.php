604c64972570ff21e1fbef7041029ac6fb3557ba
r.ratsun <r.ratsun@gmail.com>
2019-11-13 09:40:39 +0200

    79486

---
+++ app/Listeners/I18nController.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Multilang
+ * Free Extension
+ * Copyright (c) TreoLabs GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see https://www.gnu.org/licenses/.
+ */
+
+declare(strict_types=1);
+
+namespace Multilang\Listeners;
+
+use Treo\Listeners\AbstractListener;
+use Treo\Core\EventManager\Event;
+
+/**
+ * Class I18nController
+ *
+ * @author r.ratsun <r.ratsun@treolabs.com>
+ */
+class I18nController extends AbstractListener
+{
+    /**
+     * @param Event $event
+     */
+    public function afterActionRead(Event $event)
+    {
+    }
+}
